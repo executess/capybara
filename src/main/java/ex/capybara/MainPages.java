1dea8ec2cf357a09ce5ef8af09c215f0e374912b
Slava Semenoff <executss@gmail.com>
2019-02-18 11:49:53 +0200

    initial

---
+++ src/main/java/ex/capybara/MainPages.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2019. Semenoff Slava
+ */
+
+package ex.capybara;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+
+import java.util.concurrent.TimeUnit;
+public class MainPages {
+
+    protected MainTest mainTest;
+    protected WebDriver driver;
+
+    protected void isPageload(int loadTime, String errMsg) {
+        isElementPresent(By.cssSelector("body"), loadTime, errMsg);
+    }
+
+    private void isElementPresent(By locator, int waitSec, String errMsg) {
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        getElement(locator, waitSec, errMsg);
+    }
+
+    protected void waitForUrlToContainString(String URL, int waitSec, String errMsg) {
+        isPageload(waitSec, errMsg);
+        String currentUrl = driver.getCurrentUrl();
+        boolean result = currentUrl.contains(URL);
+        Assert.assertTrue(result, errMsg);
+    }
+
+    // interact with elements
+
+    protected WebElement getElement(By locator, int waitSec, String errMsg) {
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        try {
+            WebElement element = this.driver.findElement(locator);
+            driver.manage().timeouts().implicitlyWait(MainTest.DefaultDelay, TimeUnit.SECONDS);
+            return element;
+        } catch (NoSuchElementException e) {
+            System.err.print(errMsg + " | " + e.getMessage());
+            throw new WebDriverException(errMsg);
+        } finally {
+            driver.manage().timeouts().implicitlyWait(MainTest.DefaultDelay, TimeUnit.SECONDS);
+        }
+
+    }
+
+    protected void clickOnElement(By locator, int waitSec, String errMsg) {
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        WebElement element = getElement(locator, waitSec, errMsg);
+
+        WebDriverWait wait = new WebDriverWait(driver, waitSec);
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        Assert.assertTrue(element.isEnabled(), errMsg);
+        try {
+            element.click();
+        } catch (WebDriverException e) {
+            try {
+                JavascriptExecutor executor = (JavascriptExecutor) driver;
+                executor.executeScript("arguments[0].click();", element);
+            } catch (WebDriverException er) {
+                System.err.print(errMsg + " | " + er.getMessage());
+                Assert.fail(errMsg + " | " + er.getMessage(), er);
+            }
+
+        }
+    }
+    protected void isDisplayed(By locator, int waitSec, String errMsg) {
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        try {
+            WebElement element = getElement(locator, waitSec, errMsg);
+            element.isDisplayed();
+        } catch (WebDriverException e) {
+            System.err.print(errMsg + " | " + e.getMessage());
+            Assert.fail(errMsg + " | " + e.getMessage(), e);
+        } finally {
+            driver.manage().timeouts().implicitlyWait(MainTest.DefaultDelay, TimeUnit.SECONDS);
+        }
+    }
+
+    protected void isNotDisplayedAlready(final By locator, int waitSec, String errMsg) {
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+
+        WebDriverWait wait = new WebDriverWait(driver, waitSec);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        Assert.assertTrue(driver.findElement(locator).isDisplayed(), errMsg);
+
+
+
+
+
+//        WebDriverWait _wait = new WebDriverWait(driver, waitSec);
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        WebDriverWait _wait = new WebDriverWait(driver, waitSec,300);
+        ExpectedCondition elementIsDisplayed = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver arg0) {
+                try {
+                    driver.findElement(locator).isDisplayed();
+                    return false;
+                }
+                catch (NoSuchElementException e ) {
+//                    System.err.println("locator not displayed");
+                    return true;
+                }
+                catch (StaleElementReferenceException f) {
+//                    System.err.println("locator not displayed");
+                    return true;
+                }
+            }
+        };
+        _wait.until(elementIsDisplayed);
+
+//        _wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+
+//        try {
+//            Assert.assertTrue(!driver.findElement(locator).isDisplayed(), errMsg);
+//        } catch (NoSuchElementException e){
+//            Assert.assertTrue(true);
+//        }
+
+        driver.manage().timeouts().implicitlyWait(MainTest.DefaultDelay, TimeUnit.SECONDS);
+
+
+    }
+    protected void isNotVisibleAlready(final By locator, int delaySeconds, String errorMassage) {
+        driver.manage().timeouts().implicitlyWait(delaySeconds, TimeUnit.SECONDS);
+
+        WebDriverWait wait = new WebDriverWait(driver, delaySeconds);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+
+        Assert.assertTrue(driver.findElement(locator).isDisplayed(), errorMassage);
+
+        WebDriverWait _wait = new WebDriverWait(driver, delaySeconds);
+        _wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+        Assert.assertTrue(!driver.findElement(locator).isDisplayed(), errorMassage);
+        driver.manage().timeouts().implicitlyWait(MainTest.DefaultDelay, TimeUnit.SECONDS);
+
+
+    }
+
+
+    protected void clearFieldAndFillItWithText(By locator, String text, int waitSec, String errMsg){
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        WebElement element = getElement(locator,waitSec,errMsg);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected void moveTo(By locator, int waitSec, String errMsg){
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        WebElement element = getElement(locator,waitSec,errMsg);
+        Actions action = new Actions(driver);
+
+        action.moveToElement(element).perform();
+    }
+
+    protected void selectedByIndex(By locator,int index, int waitSec, String errMsg){
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        Select select = new Select(getElement(locator,waitSec,errMsg));
+        select.selectByIndex(index);
+    }
+    protected void selectedByText(By locator,String text, int waitSec, String errMsg){
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        Select select = new Select(getElement(locator,waitSec,errMsg));
+        select.selectByVisibleText(text);
+    }
+
+    protected void isTextContains(By locator,String text, int waitSec, String errMsg){
+        driver.manage().timeouts().implicitlyWait(waitSec, TimeUnit.SECONDS);
+        WebElement element = getElement(locator,waitSec,errMsg);
+        String containText = element.getText().toLowerCase();
+
+        Assert.assertTrue(containText.contains(text.toLowerCase()));
+    }
+
+    // Other functions
+
+    protected static Integer StrToInt(
+            final CharSequence input){
+        final StringBuilder sb = new StringBuilder(
+                input.length() );
+        for(int i = 0; i < input.length(); i++){
+            final char c = input.charAt(i);
+            if(c > 47 && c < 58){
+                sb.append(c);
+            }
+        }
+        String resultStr = sb.toString();
+        return Integer.parseInt(resultStr);
+    }
+}
---
+++ src/main/java/ex/capybara/MainTest.java
@@ -0,0 +1,419 @@
+/*
+ * Copyright (c) 2019. Semenoff Slava
+ */
+
+package ex.capybara;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public abstract class MainTest {
+    /*
+     * Copyright (c) 2019. Semenoff Slava
+     */
+    protected boolean isLocalRunning = true;
+    protected String SITE_URL;
+    public MainTest.Platform platform;
+    protected MainTest.BrowserType browser;
+    protected static WebDriver driver;
+    private static String driverPath = "./install/selenium//";
+    protected static int DefaultDelay = 10;
+
+    public MainTest(Platform platform){
+        this.platform = MainTest.Platform.desktop;
+        this.browser = MainTest.BrowserType.chrome;
+        this.setPlatform(platform);
+    }
+    protected enum Stages {test, stage, prod}
+
+    protected abstract String getStages(Stages stages);
+
+    protected enum BrowserType {chrome, firefox, iexplorer;}
+    protected String browser_version;
+    protected enum Platform {desktop, mobile};
+
+    protected String getSeleniumURL(){
+        return "http://localhost:4444/wd/hub"; // local selenium
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+    public void setDriver(WebDriver driver){
+        this.driver = driver;
+    }
+
+
+    public void DisableAnimations() {
+        // This script should turn off 99% of the animations and transitions in the site. Hopefully this helps settle down some animations
+        String script = "require(['jquery'], function($) {" +
+                "$(function(){" +
+                "$.fx.off = true;" +
+                "var styleEl = document.createElement('style');" +
+                "styleEl.textContent = '*{ transition: none !important; transition-property: none !important; }';" +
+                "document.head.appendChild(styleEl);" +
+                "});" +
+                "});";
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        if (jse instanceof WebDriver){
+            jse.executeScript(script);
+        }
+    }
+
+    protected void setPlatform(Platform platform){
+        this.platform = platform;
+    }
+    public Platform getPlatform(){
+        return this.platform;
+    }
+    private boolean isMobile(){
+        if (getPlatform()==Platform.mobile){
+            return true;
+        }
+        return false;
+    }
+
+    public String SiteUrl(@Optional("stage") Stages stages){
+        String site = getStages(stages);
+        System.err.println("| SITE_URL = " + site + " | ");
+        return site;
+    }
+
+    @BeforeClass
+    @Parameters({"browser","browser_version","stages"})
+    public void setup(@Optional("chrome")BrowserType browser,@Optional("72.0")String  browser_version,@Optional("stage")Stages stages){
+
+        this.browser = browser;
+        this.browser_version = browser_version;
+//        this.URL = url;
+        this.SITE_URL = SiteUrl(stages);
+    }
+
+    protected void Before(){
+
+    }
+
+
+    private void CreateRemoteDriver(DesiredCapabilities capabilities) throws MalformedURLException {
+
+        System.err.println("create "+ this.browser +" remote driver | version = "+ browser_version);
+//        capabilities.setBrowserName("chrome");
+        capabilities.setBrowserName(this.browser.toString());
+        capabilities.setVersion(this.browser_version);
+
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        this.driver = new RemoteWebDriver(
+                URI.create(this.getSeleniumURL()).toURL(),
+                capabilities
+        );
+
+    }
+
+    private void CreateLocalDriver_Chrome(boolean isMobile){
+
+        if(isMobile){
+            Map<String, String> mobileEmulation = new HashMap();
+            mobileEmulation.put("deviceName", "Nexus 5");
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+            driver = new ChromeDriver(chromeOptions);
+        }else {
+            driver = new ChromeDriver();
+        }
+    }
+    private void CreateLocalDriver_Firefox(boolean isMobile){
+
+        if(isMobile){
+//            String user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
+//
+//            Map<String, String> mobileEmulation = new HashMap();
+//            mobileEmulation.put("deviceName", "Pixel 2");
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setCapability("deviceName", "Pixel 2");
+
+//
+//            FirefoxProfile firefoxProfile
+//            firefoxProfile.setPreference("general.useragent.override", user_agent);
+//            firefoxOptions.setProfile(firefoxProfile);
+//
+
+////            profile = webdriver.FirefoxProfile()
+////            profile.set_preference("general.useragent.override", user_agent)
+////            driver = webdriver.Firefox(profile)
+////            driver.set_window_size(360,640)
+
+//            binary = FirefoxBinary('geckodriver.exe')
+//            capabilities = {
+//                    'browserName': 'firefox',
+//                    'firefoxOptions': {
+//                'mobileEmulation': {
+//                    'deviceName': 'iPhone X'
+//                }
+//            }
+//}
+//
+//            browser = webdriver.Firefox(firefox_binary=binary, desired_capabilities=capabilities)
+
+            System.out.println("mobile firefox started");
+//            firefoxOptions.setCapability("mobileEmulation","iPhone X");
+            driver = new FirefoxDriver(firefoxOptions);
+        }else {
+            driver = new FirefoxDriver();
+        }
+
+    }
+    private void CreateLocalDriver_IExplorer(boolean isMobile){
+//        CreateLocalDriver_Chrome(isMobile); // TODO temp "chrome_driver" here
+        driver = new InternetExplorerDriver();
+    }
+
+    private void DriverCreate(boolean isLocal, boolean isMobile) throws MalformedURLException {
+
+        if (isLocal) {
+            switch (this.browser) {
+                case chrome:
+                    System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+                    CreateLocalDriver_Chrome(isMobile);
+                    break;
+                case firefox:
+                    System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
+                    CreateLocalDriver_Firefox(isMobile);
+//                    driver = new FirefoxDriver();
+                    break;
+                case iexplorer:
+                    System.out.println("Delicious iexplorer");
+                    System.setProperty("webdriver.ie.driver", driverPath + "IEDriverServer.exe");
+                    CreateLocalDriver_IExplorer(isMobile);
+//
+                    break;
+            }
+        }else {
+            switch (this.browser) {
+                case chrome:
+                    if(isMobile){
+                        Map<String, String> mobileEmulation = new HashMap<>();
+
+                        mobileEmulation.put("deviceName", "Nexus 5");
+                        ChromeOptions chromeOptions = new ChromeOptions();
+                        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+                        DesiredCapabilities capabilities = new DesiredCapabilities(chromeOptions);
+                        CreateRemoteDriver(capabilities);
+                        break;
+                    }else {
+                        DesiredCapabilities capabilities = new DesiredCapabilities();
+                        CreateRemoteDriver(capabilities);
+                    }
+
+
+                    break;
+                case firefox:
+                    if(isMobile){
+                        DesiredCapabilities capabilities = new DesiredCapabilities();
+                        CreateRemoteDriver(capabilities);
+                    }else {
+                        DesiredCapabilities capabilities = new DesiredCapabilities();
+                        CreateRemoteDriver(capabilities);
+                    }
+
+                    break;
+                case iexplorer:
+                    if(isMobile){
+                        DesiredCapabilities capabilities = new DesiredCapabilities();
+                        CreateRemoteDriver(capabilities);
+                    }else {
+                        DesiredCapabilities capabilities = new DesiredCapabilities();
+                        CreateRemoteDriver(capabilities);
+                    }
+//                    this.setDriver(new InternetExplorerDriver());
+                    break;
+            }
+        }
+
+
+    }
+    /*
+
+    private boolean DriverCreate(boolean isLocal, boolean isMobile) {
+
+        if (!isLocal){
+            if(isMobile){
+                //! MOBILE VERSION
+                Map<String, String> mobileEmulation = new HashMap<>();
+
+                mobileEmulation.put("deviceName", "Nexus 5");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+                DesiredCapabilities capabilities = new DesiredCapabilities(chromeOptions);
+                try {
+                    createRemoteDriver(capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                //! DESKTOP VERSION
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                try {
+                    createRemoteDriver(capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return true;
+
+        }
+
+        if (isLocal) {
+            switch (this.browser) {
+                case chrome:
+                    if (isLocal) {
+                        System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+                        if (isMobile) {
+                            Map<String, String> mobileEmulation = new HashMap();
+                            mobileEmulation.put("deviceName", "Nexus 5");
+                            ChromeOptions chromeOptions = new ChromeOptions();
+                            chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+                            driver = new ChromeDriver(chromeOptions);
+                            return true;
+                        } else {
+                            driver = new ChromeDriver();
+                            return true;
+                        }
+                    } else {
+
+
+//
+//                    DesiredCapabilities capability = DesiredCapabilities.chrome();
+//                    capability.setBrowserName("chrome");
+//                    capability.setPlatform(org.openqa.selenium.Platform.ANY);
+//                    driver = new RemoteWebDriver(new URL(this.getSeleniumURL()), capability);
+                    }
+                    break;
+                case firefox:
+                    System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver.exe");
+                    driver = new FirefoxDriver();
+                    break;
+
+                case iexplorer:
+                    System.out.println("Delicious iexplorer");
+                    System.setProperty("webdriver.ie.driver", driverPath + "IEDriverServer.exe");
+                    this.setDriver(new InternetExplorerDriver());
+                    break;
+                default:
+                    System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver.exe");
+                    driver = new ChromeDriver();
+            }
+            return true;
+        }
+
+
+        return false;
+    }
+*/
+    @BeforeMethod(
+            description = "Initialize driver"
+    )
+    public void DriverInitialize() {
+        System.err.print("browser = " + this.browser + " | ");
+        System.err.print("platform = " + this.getPlatform() + " | ");
+
+        try {
+            this.DriverCreate(this.isLocalRunning, this.isMobile());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        this.getDriver().manage().deleteAllCookies();
+        this.getDriver().manage().window().setPosition(new Point(0, 0));
+        this.getDriver().manage().window().setSize(new Dimension(1366, 768));
+        this.getDriver().manage().timeouts().implicitlyWait((long)DefaultDelay, TimeUnit.SECONDS);
+        this.getDriver().get(this.SITE_URL);
+        this.Before();
+    }
+
+    public boolean getElement(By locator){
+        try{
+            getDriver().findElement(locator);
+            return true;
+        } catch (NoSuchElementException e){
+            return false;
+        }
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] attachment()  {
+        byte[] src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+        return src;
+    }
+    @Attachment(value = "{1}", type="text/plain")
+    public String attachmentText(String data, String description) {
+        return data;
+    }
+
+    @AfterMethod(description = "Add attachments environment | screenshot")
+    public void AfterCall(){
+        attachmentText(getEnvironment(),"Environment");
+        attachment();
+//        getDriver().close(); // close only current tab
+        getDriver().quit();
+    }
+
+
+
+    private String getEnvironment(){
+        String OS = getOS();
+        String ln = "\n";
+        System.getProperty("os.name").toLowerCase();
+
+        String osArch = System.getProperty("os.arch");
+        String javaRuntimeVersion = System.getProperty("java.runtime.version");
+
+        Capabilities cap = ((RemoteWebDriver) getDriver()).getCapabilities();
+        String browserName = cap.getBrowserName().toLowerCase();
+        String browserVersion = cap.getVersion().toString();
+        String screenResolution = getDriver().manage().window().getSize().toString();
+
+
+        return
+                "SITE_URL = " + SITE_URL + ln +
+                    "Operating system = " + OS + " | " + osArch + ln +
+                    "Screen resolution = " + screenResolution + ln +
+                    "Browser = " + browserName + " v" + browserVersion + ln +
+                    "Java version = " + javaRuntimeVersion;
+
+    }
+
+    private static String getOS () {
+        String os = System.getProperty("os.name").toLowerCase();
+        String os_version = System.getProperty("os.version").toLowerCase();
+        if (os.contains("win")) {
+            return "Windows v" + os_version;
+        } else if (os.contains("nux") || os.contains("nix")) {
+            return "Linux v" + os_version;
+        }else if (os.contains("mac")) {
+            return "Mac v" + os_version;
+        }else if (os.contains("sunos")) {
+            return "Solaris v" + os_version;
+        }else {
+            return "Other v" + os_version;
+        }
+    }
+
+
+}
